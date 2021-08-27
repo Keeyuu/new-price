6f446fee7432293be43fddaa2deaaf29617f83bb
keeYuc <52500690@qq.com>
2021-08-27 16:09:34 +0800

    add go code

--- service/dao/net/pull.go
+++ service/dao/net/pull.go
@@ -1,3 +1,17 @@
 package net
 
-func get_all_code() {}
+import (
+	"service/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+func get_all_code() {
+	engine := gin.Default()
+	engine.GET(config.Get().Net.FundCodeUrl, func(ctx *gin.Context) {
+		if ctx.Err() != nil {
+			println(ctx.Err())
+			return
+		}
+	})
+}
