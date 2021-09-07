cc122ea45caad815146e7d8c3fb447440f907797
keeYuc <52500690@qq.com>
2021-09-07 16:12:11 +0800

    add reckon

--- service/rust/src/calc/reckon.rs
+++ service/rust/src/calc/reckon.rs
@@ -1,5 +1,6 @@
 use crate::model::data;
 use anyhow::Result;
+//-----------------------------------------------------------------------
 pub struct Calc {
     data: Vec<data::Day>,
     reckoners: Vec<Box<dyn Reckoner<Input = Vec<data::Day>, Output = data::day_result>>>,
@@ -22,23 +23,29 @@ impl Calc {
     ) {
         self.reckoners.push(item);
     }
-    pub async fn save_result(self) -> Result<()> {
+    pub async fn save_result(mut self) -> Result<()> {
+        // ! fnOnce be careful
         match self.result {
             None => {
                 let mut tmp: Option<data::day_result> = None;
-                for item in self.reckoners.iter() {
-                    //tmp = Some(item.reckon(&self.data));
+                for item in self.reckoners.pop() {
+                    tmp = Some(item.reckon(&self.data));
                 }
+                if let Some(data) = tmp {
+                    data::upsert_day_result(data, self.table).await?
+                }
+                println!("{}", "err no reckoners")
             }
             Some(res) => data::upsert_day_result(res, self.table).await?,
         }
         Ok(())
     }
 }
+//-----------------------------------------------------------------------
 pub trait Reckoner {
     type Output;
     type Input;
-    fn reckon(self, i: &Self::Input) -> Self::Output;
+    fn reckon(&self, i: &Self::Input) -> Self::Output;
 }
 
 pub struct ReckonInitNode {}
@@ -46,7 +53,9 @@ pub struct ReckonInitNode {}
 impl Reckoner for ReckonInitNode {
     type Output = data::day_result;
     type Input = Vec<data::Day>;
-    fn reckon(self, i: &Self::Input) -> Self::Output {
+    fn reckon(&self, i: &Self::Input) -> Self::Output {
         Self::Output::new()
     }
 }
+
+//-----------------------------------------------------------------------
